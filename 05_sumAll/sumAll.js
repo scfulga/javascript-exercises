0b37c055422c937e41ef47a0f08bfb9ef1861b84
scfulga <146138900+scfulga@users.noreply.github.com>
2024-06-11 20:08:37 +0300

    finished problem 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let sum = 0;
+    if (first > last){
+        [first, last] = [last, first]
+    }
+    if (first < 0 || last < 0 || typeof first === 'string' || typeof last === 'string' || Array.isArray(first) || Array.isArray(last)){
+        return "ERROR";
+    }
 
+    for(let i = first; i <= last; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
