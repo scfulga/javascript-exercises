06b81929d57b68e8ee232cd62cdcffb87e2b0299
scfulga <146138900+scfulga@users.noreply.github.com>
2024-06-12 16:15:16 +0300

    finished problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+    let number = (degrees - 32) * 5/9;
+    let result = parseFloat(number.toFixed(1));
+    return result;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) { 
+    let number = (degrees * (9/5)) + 32; 
+    let result = parseFloat(number.toFixed(1));
+    return result;
+    
 };
 
 // Do not edit below this line
