6631b25def42044c6498aeac88bce9afc20399f2
scfulga <146138900+scfulga@users.noreply.github.com>
2024-07-10 13:30:34 +0300

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (string) {
+    let reversedString = '';
+    cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, ''); 
+    for (i = cleanString.length - 1; i >= 0; i--){
+        reversedString += cleanString[i];
+    }
+    return cleanString === reversedString;
+}
 // Do not edit below this line
 module.exports = palindromes;
