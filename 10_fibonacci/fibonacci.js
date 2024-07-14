220414c07feb598acd71d79e94b032f137727365
scfulga <146138900+scfulga@users.noreply.github.com>
2024-07-14 17:41:39 +0300

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num === 0) return 0;
+    if (num === 1 || num === 2) return 1; 
+    if (num < 0) return "OOPS";
 
+    fibNumbers = [1, 1];
+    currentNumber = 0;
+    
+    for (i = 2; i < num; i++){
+        currentNumber = fibNumbers[0] + fibNumbers[1];
+        fibNumbers[0] = fibNumbers[1];
+        fibNumbers[1] = currentNumber;
+    }
+    
+    return currentNumber;
 };
 
 // Do not edit below this line
