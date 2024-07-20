31ec8a63b4bb51d5049252f4b56c7a7858e8b94c
scfulga <146138900+scfulga@users.noreply.github.com>
2024-07-20 16:06:56 +0300

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0; 
+    let oldestPerson = null; 
 
+    for(i = 0; i < people.length; i++){
+        let currentPerson = people[i];
+        let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if(currentAge > oldestAge){
+            oldestPerson = currentPerson;
+            oldestAge = currentAge;
+        }
+    }
+    return oldestPerson;
 };
+
+function getAge(birth, death) {
+    if(!death) {
+        return new Date().getFullYear() - birth;
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
